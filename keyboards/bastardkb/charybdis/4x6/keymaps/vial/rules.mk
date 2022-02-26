6d87ee7ac03d49a47ccac97112d70a53747848c6
wondroushippo <wondroushippo@gmail.com>
2022-02-25 18:35:09 -0600

    Update rules.mk

--- keyboards/bastardkb/charybdis/4x6/keymaps/vial/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vial/rules.mk
@@ -1,8 +1,8 @@
 # Space saving settings.
-TAP_DANCE_ENABLE = no
-COMBO_ENABLE = no
-QMK_SETTINGS = no
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+QMK_SETTINGS = yes
 
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
-VIALRGB_ENABLE = yes
+VIALRGB_ENABLE = no
